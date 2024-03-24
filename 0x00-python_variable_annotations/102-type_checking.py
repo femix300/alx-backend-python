7ad4d4b950a1adb96dd6bdeeff141777566b4c84
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-03-24 14:32:35 +0100

    added task twelve

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''Type Checking'''
+from typing import Tuple, List, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''More annotations practice using mypy validation'''
+
+    zoomed_in: List[Any] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
+
+print(zoom_array.__annotations__)
