897f118778e8e36f095bc88fecea6ac8c2272bc7
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-03-25 04:44:51 +0100

    added task one

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''Execute multiple coroutines using async'''
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay):
+    '''
+    Calls the wait random function for n times
+    and returns a list containing its return value
+    '''
+
+    list_of_delays = []
+    for i in range(n):
+        delay = await wait_random(max_delay)
+        list_of_delays.append(delay)
+    return list_of_delays
