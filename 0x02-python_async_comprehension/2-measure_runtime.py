ddd0b50f60796c012c0cf25ae0dad2a332e1638a
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-03-25 06:46:56 +0100

    added task two

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''Run time for four parallel comprehensions'''
+import time
+import asyncio
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    '''Measure the running time'''
+    start_time = time.time()
+    tasks = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*tasks)
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time
